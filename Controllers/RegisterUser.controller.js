8248b7b5c70d094caae7854c7c4ebe7b74cc50a4
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2022-05-11 21:57:27 -0300

    testing routes

---
+++ Controllers/RegisterUser.controller.js
@@ -0,0 +1,5 @@
+module.exports = {
+    Register: async (req, res) => {
+        res.send('register user');
+    } 
+}
--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const RegisterRoute = require('./Routes/RegisterUser.route');
+app.use('/register', RegisterRoute);
+
 app.listen(PORT, () => {
     console.log("OVEM API IS RUNNING");
 })
