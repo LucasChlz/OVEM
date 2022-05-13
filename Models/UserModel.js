aaf9a7a8a49a67949abadd3516162087bd8569e4
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2022-05-13 10:16:54 -0300

    creating mongoDB connection

---
+++ Models/UserModel.js
@@ -0,0 +1,36 @@
+const { mongo } = require('../database');
+const mongoose = require('../database');
+
+const UserSchema = mongoose.Schema({
+
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    filePicture: {
+        type: String,
+        required: false
+    },
+    fileBanner: {
+        type: String,
+        required: false
+    },
+    bio: {
+        type: String,
+        required: false
+    },
+    friends: {
+        type: Number,
+        required: false
+    }
+
+
+});
