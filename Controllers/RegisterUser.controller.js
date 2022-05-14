461e7a988414815eb84bcabc64a55f9f512a2d2a
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2022-05-14 10:53:22 -0300

    verifying email format

--- Controllers/RegisterUser.controller.js
+++ Controllers/RegisterUser.controller.js
@@ -4,10 +4,18 @@ module.exports = {
     create: async (req, res) => {
         const { name, email, password } = req.body;
 
+        let formatEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        if (!formatEmail.test(email))
+        {
+            return res.send({
+                message: "this email format is invalid, please try again"
+            });
+        }
+
         const verifyEmail = await UserModel.findOne({email: email});
         if(verifyEmail)
         {
-            res.send({
+            return res.send({
                 message: "this email already used"
             });
         } else
@@ -18,7 +26,7 @@ module.exports = {
                 password: password
             });
 
-            res.send({name,email,password});
+            return res.send({name,email,password});
         }
     } 
 }
--- database.js
+++ database.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const mongoURI = process.env.mongoURI
 
 mongoose.connect("URL", {
     useNewUrlParser: true,
