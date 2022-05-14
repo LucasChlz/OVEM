523dddb2f2b2d61e3dab456d48b6639ba50b5ca1
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2022-05-14 10:28:41 -0300

    inserting basic user creation and verifying email existence

--- Controllers/RegisterUser.controller.js
+++ Controllers/RegisterUser.controller.js
@@ -1,5 +1,24 @@
+const UserModel = require('../Models/UserModel');
+
 module.exports = {
     Register: async (req, res) => {
-        res.send('register user');
+        const { name, email, password } = req.body;
+
+        const verifyEmail = await UserModel.findOne({email: email});
+        if(verifyEmail)
+        {
+            res.send({
+                message: "this email already used"
+            });
+        } else
+        {
+            await UserModel.create({
+                name: name,
+                email: email,
+                password: password
+            });
+
+            res.send({name,email,password});
+        }
     } 
 }
