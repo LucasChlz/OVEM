7e93a5f51bca15ea52a847230cb6656dac979ca8
Lucas Gabriel <lucasgabrielsampaio2019@gmail.com>
2022-05-14 09:54:36 -0300

    organizing folders and small changes

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
 
 const RegisterRoute = require('./Routes/RegisterUser.route');
 app.use('/register', RegisterRoute);
